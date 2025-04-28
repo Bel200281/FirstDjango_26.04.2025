ebff3553f5cd62b6102979763271d10ece633009
Belutkina Elena <belenka@rambler.ru>
2025-04-28 23:34:52 +0300

    Second task:at address /about/ displayed the text and deleted index.py

--- MainApp/views.py
+++ MainApp/views.py
@@ -3,9 +3,27 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+USER_DATA = {
+    'first_name': 'Иван',
+    'middle_name':'Петрович',
+    'last_name': 'Иванов',
+    'phone': '8-923-600-01-02',
+    'email': 'vasya@mail.ru'
+}
+
 def home(request):
     text = """
     <h1>"Изучаем django"</h1>
     <strong>Автор:</strong> <i>Иванов И. П.</i>
     """
     return HttpResponse(text)
+
+def  about(request):
+    html_content = f"""
+    <p>Имя: {USER_DATA['first_name']}<br>
+    Отчество: {USER_DATA['middle_name']}<br>
+    Фамилия: {USER_DATA['last_name']}<br>
+    Телефон: {USER_DATA['phone']}<br>
+    Email: {USER_DATA['email']}</p>
+    """
+    return HttpResponse(html_content)
