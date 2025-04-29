bf1ed284f2d1b1e4bdd2a19c96c9bd300479a98a
Belutkina Elena <belenka@rambler.ru>
2025-04-29 10:03:52 +0300

    Third task:list of goods

--- MainApp/views.py
+++ MainApp/views.py
@@ -10,13 +10,31 @@ USER_DATA = {
     'phone': '8-923-600-01-02',
     'email': 'vasya@mail.ru'
 }
+ITEMS = [
+    {"id": 1, "name": "Кроссовки Abibas"},
+    {"id": 2, "name": "Куртка кожаная"},
+    {"id": 3, "name": "Coca-Cola 1 литр"},
+    {"id": 4, "name": "Картофель фри"},
+    {"id": 5, "name": "Кепка"},  
+]
+
+
 
 def home(request):
     text = f"""
     <h1>Изучаем django</h1>
     <strong>Автор:</strong> <a href="/about/"><i>{USER_DATA['first_name']} {USER_DATA['middle_name']} {USER_DATA['last_name']}</i></a>
+
     """
-    return HttpResponse(text)
+    
+    items = ITEMS
+    html_content = '<h1>Товары:</h1>'
+    html_content += '<ul>'
+    for item in items:
+        html_content += f'<li><a href="/item/{item["id"]}/">{item["name"]}</a></li>'
+    html_content += '</ul>'
+    
+    return HttpResponse(text + html_content)
 
 def  about(request):
     html_content = f"""
@@ -27,3 +45,12 @@ def  about(request):
     Email: {USER_DATA['email']}</p>
     """
     return HttpResponse(html_content)
+
+def show_item(request, item_id):
+    found_items = [item for item in ITEMS if item["id"]== int(item_id)]
+    if found_items:
+        item = found_items[0]
+        return HttpResponse (f"<h2>Название товара: {item['name']}</h2>")
+    else:
+        return HttpResponse (f"Товара с номером: {item_id} не найден.", status= 404)
+    
